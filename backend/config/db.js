60fbd11c09f568c27ba1e468181f12471bfe9bfb
Waseem Hanif <f223720@cfd.nu.edu.pk>
2025-11-23 00:26:58 +0500

    Fix MongoDB env variable for Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
+    // Use the correct env variable
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
